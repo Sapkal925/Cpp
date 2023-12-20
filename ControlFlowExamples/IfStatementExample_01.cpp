ced530a7142eb095e2b6181853f1a731df26e3b1
Sapkal925 <shri.krushna925@gmail.com>
2023-12-20 15:11:35 +0530

    Control Flow Statements Examples Added

---
+++ ControlFlowExamples/IfStatementExample_01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main()
+{
+
+	std::cout<<"============================ Control Flow If Statements ========================================="<<std::endl;
+
+	int x{};
+
+	std::cout<<"1_Initial Value of x = "<<x<<std::endl;
+
+	std::cout<<"Enter any Value which needs to checkc"<<std::endl;
+	std::cin>>x;
+
+	if(x>10)
+		std::cout<<"Value Greater than 10\n";
+	else
+		std::cout<<"Value Less than 10\n";
+
+
+	return 0;
+}
